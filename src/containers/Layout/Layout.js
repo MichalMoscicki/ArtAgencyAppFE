cd00c98955cf832b33c8894189eca0a3960ddd79
Michał Mościcki <michal.moscicki.cb@gmail.com>
2023-09-02 09:13:06 +0200

    feat: layoutGrid

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -2,15 +2,26 @@ import React from "react";
 import Navbar from "./Navbar";
 import {Outlet} from "react-router-dom";
 import {Sidebar} from "./SideBar";
+import "./GridLayout.css"
 
 
 export const Layout = () => {
     return (
         <>
-            <Navbar/>
-            <Outlet/>
-            <Sidebar/>
-
+            <section>
+                <div className="layout text-2x1">
+                    <div className="navbar centered">
+                        <Navbar />
+                    </div>
+                    <div className="sidebar centered">
+                        <h1>Sidebar</h1>
+                    </div>
+                    <div className="outlet centered">
+                        <Outlet/>
+                    </div>
+                </div>
+            </section>
         </>
-    )}
+    )
+}
 
